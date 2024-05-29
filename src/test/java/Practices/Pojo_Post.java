bb1ad9b0c4a0b2654ead82ef7374d599c10d7fd9
Sandesh Bhutada <sandesh.bhutada@perficient.com>
2024-05-29 13:27:23 +0530

    First Commit

---
+++ src/test/java/Practices/Pojo_Post.java
@@ -0,0 +1,42 @@
+package Practices;
+
+public class Pojo_Post {
+	String name;
+	String place;
+	String phone;
+	String courses[];
+	String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String[] getCourses() {
+		return courses;
+	}
+	public void setCourses(String[] courses) {
+		this.courses = courses;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	
+	
+}
